387de4be405aadd9990dd7264435ff58ed4cebb4
theboyake <theboyake>
2023-04-09 12:08:14 +0500

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿// int a=1;
-// int b=2;
-// int c=a+b;
-// System.Console.WriteLine(c);
-
-
-
-// int a, b, sum;
+﻿// int a, b, sum;
 // System.Console.Write("Enter the first number >>> ");
 // a = Convert.ToInt32(Console.ReadLine());
 // System.Console.Write("Enter the second number >>> ");
@@ -35,12 +28,30 @@
 // System.Console.WriteLine(greeting);
 
 
-System.Console.Write("Enter your age >>> ");
-string ageAsString = Console.ReadLine();
-System.Console.WriteLine("Converting ...");
-int age = Convert.ToInt32(ageAsString);
-System.Console.WriteLine($"Succesfully converted {age}");
+// System.Console.Write("Enter your age >>> ");
+// string ageAsString = Console.ReadLine();
+// System.Console.WriteLine("Converting ...");
+// int age = Convert.ToInt32(ageAsString);
+// System.Console.WriteLine($"Succesfully converted {age}");
 
-int keschaAge = 3;
-int ageDifference = age - keschaAge;
-System.Console.WriteLine($"The difference between your and Kescha's age {ageDifference}");
+// int keschaAge = 3;
+// int ageDifference = age - keschaAge;
+// System.Console.WriteLine($"The difference between your and Kescha's age {ageDifference}");
+
+
+
+
+// System.Console.Write("1-raqamni kiriting >>> ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("2-raqamni kiriting >>> ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine($"1-son 2-sondan katta: {firstNumber > secondNumber}");
+// System.Console.WriteLine($"1-son 2-sondan katta yoki teng: {firstNumber >= secondNumber}");
+// System.Console.WriteLine($"1-son 2-sondan kichik: {firstNumber < secondNumber}");
+// System.Console.WriteLine($"1-son 2-sondan kichik yoki teng: {firstNumber <= secondNumber}");
+// System.Console.WriteLine($"Sonlar teng: {firstNumber == secondNumber}");
+// System.Console.WriteLine($"Sonlar teng emas: {firstNumber != secondNumber}");
+
+
+
